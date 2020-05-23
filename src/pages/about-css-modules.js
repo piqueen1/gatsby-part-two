6bc25aeaf53899f9fe8ad5d3dc4f86f39bcaf992
Finn Thye <piqueen@gmail.com>
2020-05-22 19:42:46 -0600

    Finish part 2

--- src/pages/about-css-modules.js
+++ src/pages/about-css-modules.js
@@ -1,14 +1,34 @@
 import React from "react"
-import Styles from "./about-css-modules.module.css"
+import styles from "./about-css-modules.module.css"
 import Container from "../components/container"
 
-console.log(Styles)
+console.log(styles)
+
+const User = props => (
+  <div className={styles.user}>
+    <img src={props.avatar} className={styles.avatar} alt="" />
+    <div className={styles.description}>
+      <h2 className={styles.username}>{props.username}</h2>
+      <p className={styles.excerpt}>{props.excerpt}</p>
+    </div>
+  </div>
+)
 
 export default function About() {
   return (
     <Container>
       <h1>About CSS Modules</h1>
       <p>CSS Modules are cool</p>
+      <User
+        username="Jane Doe"
+        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+        excerpt="I'm Jane Doe, or some such person, in a vague but inspiring way"
+      />
+      <User
+        username="Billy Joel"
+        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+        excerpt="I'm a singer who makes you feel sort of nostalgic and sort of relieved at the same time."
+      />
     </Container>
   )
 }
